39639962652d3feb788c3bb0dcfd1ce127c38726
Cooper <dancersoftaranto@gmail.com>
2023-03-07 15:14:12 +0300

    deneme

--- src/components/EditTutorial.jsx
+++ src/components/EditTutorial.jsx
@@ -1,72 +1,72 @@
 import { useEffect, useState } from "react";
 
 const EditTutorial = ({ edititem, editTutorial }) => {
-  const { id, title, description } = edititem;
-  const [newtitle, setNewtitle] = useState(title);
-  const [newdescription, setNewdescription] = useState(description);
+    const { id, title, description } = edititem;
+    const [newtitle, setNewtitle] = useState(title);
+    const [newdescription, setNewdescription] = useState(description);
 
-  useEffect(() => {
-    setNewtitle(title);
-    setNewdescription(description);
-  }, [title, description]);
+    useEffect(() => {
+        setNewtitle(title);
+        setNewdescription(description);
+    }, [title, description]);
 
-  return (
-      <div>
-          <div className="modal fade" id="edit-modal" tabIndex={-1}>
-              <div className="modal-dialog">
-                  <div className="modal-content">
-                      <div className="modal-header">
-                          <h1
-                              className="modal-title fs-5"
-                              id="exampleModalLabel"
-                          >
-                              Edit Tutorial
-                          </h1>
-                      </div>
+    return (
+        <div>
+            <div className="modal fade" id="edit-modal" tabIndex={-1}>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1
+                                className="modal-title fs-5"
+                                id="exampleModalLabel"
+                            >
+                                Edit Tutorial
+                            </h1>
+                        </div>
 
-                      <div className="modal-body">
-                          <p className="text-danger">Title : </p>
-                          <input
-                              type="text"
-                              value={newtitle}
-                              onChange={(e) => setNewtitle(e.target.value)}
-                              className="border border-2 rounded-2 p-2"
-                          />
-                          <br /> <br />
-                          <p className="text-danger">Description : </p>
-                          <input
-                              type="text"
-                              value={newdescription}
-                              onChange={(e) =>
-                                  setNewdescription(e.target.value)
-                              }
-                              className="border border-2 rounded-2 p-2"
-                          />
-                      </div>
-                      <div className="modal-footer">
-                          <button
-                              type="button"
-                              className="btn btn-danger"
-                              data-bs-dismiss="modal"
-                          >
-                              Close
-                          </button>
-                          <button
-                              type="button"
-                              className="btn btn-success"
-                              data-bs-dismiss="modal"
-                              onClick={() =>
-                                  editTutorial(id, newtitle, newdescription)
-                              }
-                          >
-                              Save Changes
-                          </button>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  );
+                        <div className="modal-body">
+                            <p className="text-danger">Title : </p>
+                            <input
+                                type="text"
+                                value={newtitle}
+                                onChange={(e) => setNewtitle(e.target.value)}
+                                className="border border-2 rounded-2 p-2"
+                            />
+                            <br /> <br />
+                            <p className="text-danger">Description : </p>
+                            <input
+                                type="text"
+                                value={newdescription}
+                                onChange={(e) =>
+                                    setNewdescription(e.target.value)
+                                }
+                                className="border border-2 rounded-2 p-2"
+                            />
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-danger"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-success"
+                                data-bs-dismiss="modal"
+                                onClick={() =>
+                                    editTutorial(id, newtitle, newdescription)
+                                }
+                            >
+                                Save Changes
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default EditTutorial;
--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -5,88 +5,88 @@ import EditTutorial from "./EditTutorial";
 import { useState } from "react";
 
 const TutorialList = ({ tutor, getTutorials }) => {
-  const [edititem, setEdititem] = useState([]);
+    const [edititem, setEdititem] = useState([]);
 
-  //! DELETE (CRUD-Delete)
-  const deleteTutorial = async (id) => {
-    const url = "http://coopersky.pythonanywhere.com/tutorials";
-    try {
-      await axios.delete(`${url}/${id}/`);
-      // id vermez isek var olan bütün verileri siler,
-      // url ve id yi beraber yazabilmek için backtick içinde yazıyoruz.
-      // axios yazarsak Get yapar default olarak, onun için başına delete yazıyoruz.
-    } catch (error) {
-      console.log(error);
-    }
-    getTutorials();
-    // sildikten sonra sayfanın refresh etmeden render olması için, home componenti içinden getTutorials() fonksiyonunu prop olarak yollayıp burada karşılıyor ve sonra çağırıyoruz.
-    //! <TutorialList tutor={tutorials} getTutorials={getTutorials} />
-  };
+    //! DELETE (CRUD-Delete)
+    const deleteTutorial = async (id) => {
+        const url = "http://coopersky.pythonanywhere.com/tutorials";
+        try {
+            await axios.delete(`${url}/${id}/`);
+            // id vermez isek var olan bütün verileri siler,
+            // url ve id yi beraber yazabilmek için backtick içinde yazıyoruz.
+            // axios yazarsak Get yapar default olarak, onun için başına delete yazıyoruz.
+        } catch (error) {
+            console.log(error);
+        }
+        getTutorials();
+        // sildikten sonra sayfanın refresh etmeden render olması için, home componenti içinden getTutorials() fonksiyonunu prop olarak yollayıp burada karşılıyor ve sonra çağırıyoruz.
+        //! <TutorialList tutor={tutorials} getTutorials={getTutorials} />
+    };
 
-  //! PUT (CRUD-Update)
-  //! PUT: Whole Update, PATCH: Partially Update
-  const editTutorial = async (id, title, description) => {
-    // hangi tutorial değişecek--> id, başlık ne olacak-->title, açıklama ne olacak--> description
-    // const { id, title, description } = item;
-    const url = "http://coopersky.pythonanywhere.com/tutorials";
-    try {
-      await axios.put(`${url}/${id}/`, { title, description });
-      // 2 parametre alıyor, url(id eklnemiş olarak) ve değişecek key/value objesi
-    } catch (error) {
-      console.log(error);
-    }
-    getTutorials();
-  };
+    //! PUT (CRUD-Update)
+    //! PUT: Whole Update, PATCH: Partially Update
+    const editTutorial = async (id, title, description) => {
+        // hangi tutorial değişecek--> id, başlık ne olacak-->title, açıklama ne olacak--> description
+        // const { id, title, description } = item;
+        const url = "http://coopersky.pythonanywhere.com/tutorials";
+        try {
+            await axios.put(`${url}/${id}/`, { title, description });
+            // 2 parametre alıyor, url(id eklnemiş olarak) ve değişecek key/value objesi
+        } catch (error) {
+            console.log(error);
+        }
+        getTutorials();
+    };
 
-  return (
-    <div className="container mt-4">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Description</th>
-            <th scope="col" className="text-center">
-              Edit & Delete
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {tutor?.map((item) => {
-            const { id, title, description } = item;
-            return (
-              <tr key={id}>
-                <th>{id}</th>
-                <td>{title}</td>
-                <td>{description}</td>
-                <td className="text-center text-nowrap">
-                  <FaEdit
-                    size={20}
-                    type="button"
-                    data-bs-toggle="modal"
-                    data-bs-target="#edit-modal"
-                    // edit modal componentine bağlantı yaptı.
-                    // çalışması için index.html bootstrap script eklemek gerekli
-                    className="me-2 text-success"
-                    onClick={() => setEdititem(item)}
-                  />
-                  <AiFillDelete
-                    size={22}
-                    type="button"
-                    className="text-danger "
-                    onClick={() => deleteTutorial(id)}
-                    //  id zaten yukarıdan dest edilmişti, parametre olarak verince arrow func çevirmek gerekli
-                  />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    return (
+        <div className="container mt-4">
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col" className="text-center">
+                            Edit & Delete
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tutor?.map((item) => {
+                        const { id, title, description } = item;
+                        return (
+                            <tr key={id}>
+                                <th>{id}</th>
+                                <td>{title}</td>
+                                <td>{description}</td>
+                                <td className="text-center text-nowrap">
+                                    <FaEdit
+                                        size={20}
+                                        type="button"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#edit-modal"
+                                        // edit modal componentine bağlantı yaptı.
+                                        // çalışması için index.html bootstrap script eklemek gerekli
+                                        className="me-2 text-success"
+                                        onClick={() => setEdititem(item)}
+                                    />
+                                    <AiFillDelete
+                                        size={22}
+                                        type="button"
+                                        className="text-danger "
+                                        onClick={() => deleteTutorial(id)}
+                                        //  id zaten yukarıdan dest edilmişti, parametre olarak verince arrow func çevirmek gerekli
+                                    />
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
 
-      <EditTutorial edititem={edititem} editTutorial={editTutorial} />
-    </div>
-  );
+            <EditTutorial edititem={edititem} editTutorial={editTutorial} />
+        </div>
+    );
 };
 
 export default TutorialList;
